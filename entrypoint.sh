5c7a6cf4e9772e6c1f5655d59cdcbb75337d7945
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-03-19 00:57:52 +0100

    gogogo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
-docker run --platform linux/amd64 -d --name eventbus-e2e-eventstore -it -p 2113:2113 -p 1113:1113 eventstore/eventstore:release-5.0.8
+docker run -d --name eventstore-eventbus-E2E -it -p 2113:2113 -p 1113:1113 eventstore/eventstore:release-5.0.8 --insecure --run-projections=All
+
 #docker run --platform linux/amd64 -d --name eventbus-e2e-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 #docker run --platform linux/amd64 -d --name eventbus-e2e-redis -d redis
