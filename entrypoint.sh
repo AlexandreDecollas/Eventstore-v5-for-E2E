a1f43a1d5aa01aed88d5cf281d36a9273bbe4f8c
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-03-21 08:50:12 +0100

    add eventstore for interco

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
-docker run -d --name eventstore-eventbus-E2E -it -p 2113:2113 -p 1113:1113 eventstore/eventstore:release-5.0.8 --insecure --run-projections=All --enable-atom-pub-over-http
+docker run -d --name eventstore-eventbus-E2E-main -it -p 2113:2113 -p 1113:1113 eventstore/eventstore:release-5.0.8 --insecure --run-projections=All --enable-atom-pub-over-http
+
+docker run -d --name eventstore-eventbus-E2E-interco-helper -it -p 21131:21131 -p 11131:11131 eventstore/eventstore:release-5.0.8 --insecure --run-projections=All --enable-atom-pub-over-http
 
 docker run --platform linux/amd64 -d --name eventbus-e2e-postgres -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 
