6486364ff073c963ae414a7669000bc7fae12dc2
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-03-21 08:37:52 +0100

    add ports for redis and postres dockers

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 docker run -d --name eventstore-eventbus-E2E -it -p 2113:2113 -p 1113:1113 eventstore/eventstore:release-5.0.8 --insecure --run-projections=All --enable-atom-pub-over-http
 
-docker run --platform linux/amd64 -d --name eventbus-e2e-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+docker run --platform linux/amd64 -d --name eventbus-e2e-postgres -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 
-docker run --platform linux/amd64 -d --name eventbus-e2e-redis -d redis
+docker run --platform linux/amd64 -d --name eventbus-e2e-redis -p 2113:2113 -d redis
